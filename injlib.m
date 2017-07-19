36833aa23c44f3c340456db14eb71d1f1dc676aa
Erona <erona@loli.bz>
2017-07-19 17:57:14 +0800

    add comments

--- injlib.m
+++ injlib.m
@@ -10,21 +10,28 @@
 {
     NSLog(@"------ [ injlib ] ------\n");
     NSLog(@"injlib.dylib have already injected.");
+    // get class Test
     Class clsTest = NSClassFromString(@"Test");
     NSLog(@"clsTest: %p\n",clsTest);
+    // get methods
     Method originalMethod = class_getInstanceMethod(clsTest,@selector(test:));
     Method newMethod = class_getInstanceMethod(self,@selector(test:));
+    // get imps
     originalImp = method_getImplementation(originalMethod);
     IMP newImp = method_getImplementation(newMethod);
+    // set new imp to original method
     method_setImplementation(originalMethod,newImp);
 }
 
 IMP originalImp;
 int a=100;
+
 - (int) test:(int) x
 {
     NSLog(@"[Injection test:]\n");
-    NSLog(@"original method returns: %i\n",(int)originalImp(self,@selector(test),x));
+    // call original method by original imp
+    int ret = (int)originalImp(self,@selector(test),x);
+    NSLog(@"original method returns: %i\n", ret);
     return x+a;
 }
 @end
